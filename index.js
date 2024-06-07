8ab9e77e4c2fc4e1d8e208e73264a095912aec39
Matias Aramayo <matiasaramayo@hotmail.com>
2024-06-07 17:45:25 -0300

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Middleware
 app.use(bodyParser.json());
